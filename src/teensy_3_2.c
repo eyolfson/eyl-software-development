99d7af2da19d3d9cf95e5c597a9e7b87ab3f9b9e
Jon Eyolfson <jon@eyl.io>
2017-03-14 10:23:35 -0400

    End of usb table init

--- src/teensy_3_2.c
+++ src/teensy_3_2.c
@@ -2852,7 +2852,7 @@ void teensy_3_2_emulate(uint8_t *data, uint32_t length) {
 	}
 
 	printf("\nExecution:\n");
-	for (int i = 0; i < 3749; ++i){
+	for (int i = 0; i < 3901; ++i){
 		step(&registers);
 	}
 }
